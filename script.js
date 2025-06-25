981ca29df40af345c31febf6c6c14c37e6ecc88a
bojal <108387883+bojal@users.noreply.github.com>
2025-06-24 20:36:20 -0400

    Proyectos

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ const projects = [
    {
         title: "cliquer",
         description: "Juego incremental hecho en JavaScript.",
-        imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdummyimage.com%2F&psig=AOvVaw3S4TyQwZbZoz0qfWaqUOzv&ust=1750896194620000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJDg6uOii44DFQAAAAAdAAAAABAE",
+        imageUrl: "https://placehold.co/600x400",
         projectUrl: "https://bojal.github.io/clicker_b/"
     },
     {
-        title: "Proyecto 2",
-        description: "Descripción breve del segundo proyecto y los desafíos superados.",
+        title: "Geobubs",
+        description: "Juego para gamejam de burbujas.",
         imageUrl: "https://via.placeholder.com/600x400",
-        projectUrl: "#"
+        projectUrl: "https://bojal.itch.io/geobubs"
     },
     {
-        title: "Proyecto 3",
-        description: "Descripción breve del tercer proyecto y los resultados obtenidos.",
+        title: "DarkDead",
+        description: "Juego hecho en 2do semestre de carrera",
         imageUrl: "https://via.placeholder.com/600x400",
-        projectUrl: "#"
+        projectUrl: "https://bojal.itch.io/darkdead"
     },
     {
         title: "Proyecto 4",
