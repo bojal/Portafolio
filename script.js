6c26cc350e4c098eeec36075e3d4c16fd125e8d5
bojal <108387883+bojal@users.noreply.github.com>
2025-06-24 20:04:10 -0400

    imagev2

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const projects = [
-    {
-        title: "Proyecto 1",
-        description: "Descripción breve del primer proyecto y las tecnologías utilizadas.",
-        imageUrl: "https://via.placeholder.com/600x400",
-        projectUrl: "#"
+   {
+        title: "cliquer",
+        description: "Juego incremental hecho en JavaScript.",
+        imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdummyimage.com%2F&psig=AOvVaw3S4TyQwZbZoz0qfWaqUOzv&ust=1750896194620000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJDg6uOii44DFQAAAAAdAAAAABAE",
+        projectUrl: "https://bojal.github.io/clicker_b/"
     },
     {
         title: "Proyecto 2",
@@ -27,19 +27,23 @@ const projects = [
 
 function generatePortfolioCards() {
     const container = document.getElementById('portfolio-container');
-    
+
     projects.forEach(project => {
         const card = document.createElement('div');
         card.className = 'portfolio-card';
-        
+        card.style.cursor = 'pointer';
+        card.onclick = () => {
+            window.open(project.projectUrl, '_blank');
+        };
+
         card.innerHTML = `
-            <img src="${project.imageUrl}" alt="${project.title}" class="card-image" onclick="window.location.href='${project.projectUrl}'">
+            <img src="${project.imageUrl}" alt="${project.title}" class="card-image">
             <div class="card-content">
                 <h3>${project.title}</h3>
                 <p>${project.description}</p>
             </div>
         `;
-        
+
         container.appendChild(card);
     });
 }
